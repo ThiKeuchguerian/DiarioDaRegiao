181bac67a27cc2c0a69cda07626a822df7f9dad4
Thiago Guimarães Corrêa Keuchguerian <thi.guimaraes@yahoo.com.br>
2025-05-28 09:28:34 -0300

    Ajustado layout do modal deixando os campos com fundo cinza que não pode ser editados.

--- public/includes/modals/fin_VerCentroCustFam.php
+++ public/includes/modals/fin_VerCentroCustFam.php
@@ -2,7 +2,7 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" style="font-weight: bold;" id="modal">Editar Pedido - <?= $item['numped'] ?></h5>
+        <h5 class="modal-title" style="font-weight: bold;" id="modal">Editar Pedido</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <form action="<?= $URL ?>" method="post">
@@ -11,15 +11,15 @@
           <div class="row mb-3">
             <div class="col">
               <label for="numped" class="form-label" style="font-weight: bold;">Cod. Pedido</label>
-              <input type="text" class="form-control" id="numped" name="numped" readonly>
+              <input type="text" class="form-control" id="numped" name="numped" style="background-color: #e9ecef;" readonly>
             </div>
             <div class="col">
               <label for="pedcli" class="form-label" style="font-weight: bold;">Cod. NumPed</label>
-              <input type="text" class="form-control" id="pedcli" name="pedcli" readonly>
+              <input type="text" class="form-control" id="pedcli" name="pedcli" style="background-color: #e9ecef;" readonly>
             </div>
             <div class="col">
               <label for="codcli" class="form-label" style="font-weight: bold;">Cod. Cliente</label>
-              <input type="text" class="form-control" id="codcli" name="codcli" readonly>
+              <input type="text" class="form-control" id="codcli" name="codcli" style="background-color: #e9ecef;" readonly>
             </div>
           </div>
           <div class="row mb-3">
@@ -39,24 +39,24 @@
           <div class="row mb-3">
             <div class="col">
               <label for="codfam" class="form-label" style="font-weight: bold;">Código da Família</label>
-              <input type="text" class="form-control" id="codfam" name="codfam" readonly>
+              <input type="text" class="form-control" id="codfam" name="codfam" style="background-color: #e9ecef;" readonly>
             </div>
             <div class="col">
               <label for="codser" class="form-label" style="font-weight: bold;">Código do Serviço</label>
-              <input type="text" class="form-control" id="codser" name="codser" readonly>
+              <input type="text" class="form-control" id="codser" name="codser" style="background-color: #e9ecef;" readonly>
             </div>
             <div class="col">
               <label for="tnsser" class="form-label" style="font-weight: bold;">Transação Serviço</label>
-              <input type="text" class="form-control" id="tnsser" name="tnsser" readonly>
+              <input type="text" class="form-control" id="tnsser" name="tnsser" style="background-color: #e9ecef;" readonly>
             </div>
           </div>
           <div class="mb-3">
             <label for="nomcli" class="form-label" style="font-weight: bold;">Cliente</label>
-            <input type="text" class="form-control" id="nomcli" name="nomcli" readonly>
+            <input type="text" class="form-control" id="nomcli" name="nomcli" style="background-color: #e9ecef;" readonly>
           </div>
           <div class="mb-3">
             <label for="desser" class="form-label" style="font-weight: bold;">Descrição</label>
-            <input type="text" class="form-control" id="desser" name="desser" readonly>
+            <input type="text" class="form-control" id="desser" name="desser" style="background-color: #e9ecef;" readonly>
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
