4ec996519365dabb413a4e32e1d0ea54adad13a4
Thiago Guimarães Corrêa Keuchguerian <thi.guimaraes@yahoo.com.br>
2025-06-03 16:37:13 -0300

    Incluido função depurar

--- public/config/config.php
+++ public/config/config.php
@@ -11,3 +11,12 @@ define('URL_PRINCIPAL', 'http://devsistemas.diariodaregiao.com.br/');
 define('SITE_TITLE',    'Grupo Diário da Região');
 define('LOGO',          '/config/../img/logo_write.svg');
 define('FAVICON',       '/config/../img/favicon.ico');
+
+// Função global para depuração
+function depurar($var)
+{
+  echo '<pre>';
+  var_dump($var);  
+  echo '</pre>';
+  die();
+}
