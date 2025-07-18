31e77307a6292092c4140c7a4941dbc84f7441fa
Thiago Guimarães Corrêa Keuchguerian <thi.guimaraes@yahoo.com.br>
2025-07-18 16:45:48 -0300

    incluido informação do conta diario

--- public/analitico/Circulacao.php
+++ public/analitico/Circulacao.php
@@ -9,72 +9,73 @@ $URL = URL_PRINCIPAL . 'analitico/Circulacao.php';
 $AnaliticoCirculacao = new AnaliticoCirculacao();
 
 if (isset($_POST['btn-geral'])) {
-  $consultaDados = $AnaliticoCirculacao->consultaContratos();
-  // $consultaDados = [
-  //   [
-  //     'Tipo' => 'Combo',
-  //     'Produto' => 3,
-  //     'DataInicio' => '2025-07-01',
-  //     'DataFinal' => '2026-07-01',
-  //     'CpfCnpj' => '12345678901',
-  //     'NomeRazaoSocial' => 'João da Silva',
-  //     'Cidade' => 'São José do Rio Preto',
-  //     'UF' => 'SP',
-  //     'diaDeNascimento' => 15,
-  //     'mesDeNascimento' => 5,
-  //     'anoDeNascimento' => 1980,
-  //     'sexo' => 'M',
-  //     'CodSituacao' => 1,
-  //     'SituacaoContrato' => 'Ativo',
-  //     'DataAssinatura' => '2025-06-28',
-  //     'ValorContrato' => 480.00,
-  //     'ValorParcela' => 40.00,
-  //     'TipoAssinatura' => 'Mensal',
-  //     'PlanoPagto' => 'CBO - Combo Digital + Impresso'
-  //   ],
-  //   [
-  //     'Tipo' => 'Impresso',
-  //     'Produto' => 1,
-  //     'DataInicio' => '2025-07-01',
-  //     'DataFinal' => '2026-07-01',
-  //     'CpfCnpj' => '98765432100',
-  //     'NomeRazaoSocial' => 'Maria Oliveira',
-  //     'Cidade' => 'Catanduva',
-  //     'UF' => 'SP',
-  //     'diaDeNascimento' => 3,
-  //     'mesDeNascimento' => 11,
-  //     'anoDeNascimento' => 1975,
-  //     'sexo' => 'F',
-  //     'CodSituacao' => 3,
-  //     'SituacaoContrato' => 'Suspenso',
-  //     'DataAssinatura' => '2025-06-30',
-  //     'ValorContrato' => 360.00,
-  //     'ValorParcela' => 30.00,
-  //     'TipoAssinatura' => 'Mensal',
-  //     'PlanoPagto' => 'Plano Impresso Mensal'
-  //   ],
-  //   [
-  //     'Tipo' => 'Digital',
-  //     'Produto' => 3,
-  //     'DataInicio' => '2025-07-05',
-  //     'DataFinal' => '2026-07-05',
-  //     'CpfCnpj' => '11223344556',
-  //     'NomeRazaoSocial' => 'Carlos Lima',
-  //     'Cidade' => 'Mirassol',
-  //     'UF' => 'SP',
-  //     'diaDeNascimento' => 22,
-  //     'mesDeNascimento' => 9,
-  //     'anoDeNascimento' => 1990,
-  //     'sexo' => 'M',
-  //     'CodSituacao' => 1,
-  //     'SituacaoContrato' => 'Ativo',
-  //     'DataAssinatura' => '2025-07-03',
-  //     'ValorContrato' => 240.00,
-  //     'ValorParcela' => 20.00,
-  //     'TipoAssinatura' => 'Mensal',
-  //     'PlanoPagto' => 'Plano Digital Mensal'
-  //   ],
-  // ];
+  // $consultaDados = $AnaliticoCirculacao->consultaContratos();
+  $consultaContaDiario = $AnaliticoCirculacao->consultaContaDiario();
+  $consultaDados = [
+    [
+      'Tipo' => 'Combo',
+      'Produto' => 3,
+      'DataInicio' => '2025-07-01',
+      'DataFinal' => '2026-07-01',
+      'CpfCnpj' => '12345678901',
+      'NomeRazaoSocial' => 'João da Silva',
+      'Cidade' => 'São José do Rio Preto',
+      'UF' => 'SP',
+      'diaDeNascimento' => 15,
+      'mesDeNascimento' => 5,
+      'anoDeNascimento' => 1980,
+      'sexo' => 'M',
+      'CodSituacao' => 1,
+      'SituacaoContrato' => 'Ativo',
+      'DataAssinatura' => '2025-06-28',
+      'ValorContrato' => 480.00,
+      'ValorParcela' => 40.00,
+      'TipoAssinatura' => 'Mensal',
+      'PlanoPagto' => 'CBO - Combo Digital + Impresso'
+    ],
+    [
+      'Tipo' => 'Impresso',
+      'Produto' => 1,
+      'DataInicio' => '2025-07-01',
+      'DataFinal' => '2026-07-01',
+      'CpfCnpj' => '98765432100',
+      'NomeRazaoSocial' => 'Maria Oliveira',
+      'Cidade' => 'Catanduva',
+      'UF' => 'SP',
+      'diaDeNascimento' => 3,
+      'mesDeNascimento' => 11,
+      'anoDeNascimento' => 1975,
+      'sexo' => 'F',
+      'CodSituacao' => 3,
+      'SituacaoContrato' => 'Suspenso',
+      'DataAssinatura' => '2025-06-30',
+      'ValorContrato' => 360.00,
+      'ValorParcela' => 30.00,
+      'TipoAssinatura' => 'Mensal',
+      'PlanoPagto' => 'Plano Impresso Mensal'
+    ],
+    [
+      'Tipo' => 'Digital',
+      'Produto' => 3,
+      'DataInicio' => '2025-07-05',
+      'DataFinal' => '2026-07-05',
+      'CpfCnpj' => '11223344556',
+      'NomeRazaoSocial' => 'Carlos Lima',
+      'Cidade' => 'Mirassol',
+      'UF' => 'SP',
+      'diaDeNascimento' => 22,
+      'mesDeNascimento' => 9,
+      'anoDeNascimento' => 1990,
+      'sexo' => 'M',
+      'CodSituacao' => 1,
+      'SituacaoContrato' => 'Ativo',
+      'DataAssinatura' => '2025-07-03',
+      'ValorContrato' => 240.00,
+      'ValorParcela' => 20.00,
+      'TipoAssinatura' => 'Mensal',
+      'PlanoPagto' => 'Plano Digital Mensal'
+    ],
+  ];
   $geral = count(array_unique(array_column($consultaDados, 'CpfCnpj')));
 
   // Inicializar contadores
@@ -95,6 +96,7 @@ if (isset($_POST['btn-geral'])) {
     'Mais de 70 anos' => 0
   ];
 
+  $TotalContaDiario = count($consultaContaDiario);
   $clientesContabilizados = []; // ← controle de duplicidade
 
   foreach ($consultaDados as $linha) {
@@ -164,10 +166,42 @@ if (isset($_POST['btn-geral'])) {
       $combo++;
     }
   }
+
+  $tipo = 0;
+  $assDigital = 0;
+  $assHolos = 0;
+  $tipoAss = 0;
+  $assAnual = 0;
+  $assMensal = 0;
+  foreach ($consultaContaDiario as $item) {
+    // depurar($item);
+    $tipo = ($item['type']);
+    if ($tipo == 'paid_pass') {
+      $assDigital++;
+    } elseif ($tipo == 'free_pass') {
+      $assHolos++;
+    }
+    if ($tipo == 'paid_pass') {
+      $tipoAss = ($item['name']);
+      if ($tipoAss == 'Plano Anual / Digital') {
+        $assAnual++;
+      } elseif ($tipoAss == 'Plano Mensal / Digital') {
+        $assMensal++;
+      }
+    }
+  }
 } else if (isset($_POST['btn-analitico'])) {
   $consultaDados = $AnaliticoCirculacao->consultaContratos();
-  $Total = count($consultaDados);
-  $TotalClietnes = count(array_unique(array_column($consultaDados, 'CpfCnpj')));
+  $TotalCliHolos = count($consultaDados);
+  $TotalClientes = count(array_unique(array_column($consultaDados, 'CpfCnpj')));
+
+  $consultaContaDiario = $AnaliticoCirculacao->consultaContaDiario();
+  $TotalCliContaD = count($consultaContaDiario);
+  $TotalClientesCD = count(array_filter($consultaContaDiario, function ($item) {
+    return isset($item['type']) && $item['type'] === 'paid_pass';
+  }));
+
+  $Total = count($consultaDados) + count($consultaContaDiario);
 }
 
 // Inclui o header da página
@@ -206,8 +240,16 @@ require_once __DIR__ . '/../includes/header.php';
 <!-- Exibindo Resultado Geral -->
 <?php if (!empty($geral)) : ?>
   <div class="container my-6">
-    <div class="row g-4">
-
+    <div class="row g-4 justify-content-center text-center">
+      <!-- Total de Clientes -->
+      <div class="col-md-3 col-lg-2">
+        <div class="card shadow-sm border-0 rounded-3 p-3 bg-light">
+          <div class="text-center">
+            <h6 class="text-uppercase text-muted mb-1 fw-bold">Total Geral</h6>
+            <h4 class="fw-bold text-black"><?= ($geral + $assDigital) ?></h4>
+          </div>
+        </div>
+      </div>
       <!-- Total de Clientes -->
       <div class="col-md-3 col-lg-2">
         <div class="card shadow-sm border-0 rounded-3 p-3 bg-light">
@@ -257,6 +299,48 @@ require_once __DIR__ . '/../includes/header.php';
           </div>
         </div>
       </div>
+      <br>
+      <!-- Conta Diario -->
+      <!-- Total de Clientes -->
+      <div class="col-md-3 col-lg-2">
+        <div class="card shadow-sm border-0 rounded-3 p-3 bg-light">
+          <div class="text-center">
+            <h6 class="text-uppercase text-muted mb-1 fw-bold">Total Clientes</h6>
+            <h4 class="fw-bold text-black"><?= $TotalContaDiario ?></h4>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-5 col-lg-4">
+        <div class="card shadow-sm border-0 rounded-3 p-3 bg-light">
+          <div class="row">
+            <div class="col text-center border-end">
+              <h6 class="text-muted mb-1 fw-bold">Conta Diário</h6>
+              <h5 class="fw-bold text-primary"><?= $assDigital ?></h5>
+            </div>
+            <div class="col text-center">
+              <h6 class="text-muted mb-1 fw-bold">Holos</h6>
+              <h5 class="fw-bold text-success"><?= $assHolos ?></h5>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-5 col-lg-4">
+        <div class="card shadow-sm border-0 rounded-3 p-3 bg-light">
+          <div class="row">
+            <div class="col text-center border-end">
+              <h6 class="text-muted mb-1 fw-bold">Plano Anual</h6>
+              <h5 class="fw-bold text-primary"><?= $assAnual ?></h5>
+            </div>
+            <div class="col text-center">
+              <h6 class="text-muted mb-1 fw-bold">Plano Mensal</h6>
+              <h5 class="fw-bold text-success"><?= $assMensal ?></h5>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
     <div class="row mt-2">
       <div class="col-md-6">
@@ -281,56 +365,104 @@ require_once __DIR__ . '/../includes/header.php';
 
 <!-- Exibindo Resultado Analítico -->
 <?php if (!empty($Total)) : ?>
-  <div class="container">
-    <div class="col col-sm-12">
-      <div class="card shadow-sm">
-        <div class="card-body">
-          <h5 class="card-header bg-primary text-white">
-            Qtde. Total: <?= $Total ?> ||
-            Total Clientes: <?= $TotalClietnes ?>
+  <div class="container mt-4">
+    <div class="accordion" id="accordionContaDiario">
+      <div class="accordion-item border rounded shadow-sm mb-3">
+        <h2 class="accordion-header" id="headingOne">
+          <button class="accordion-button collapsed bg-primary text-white fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+            <i class="bi bi-card-list me-2"></i>
+            Relação Clientes Conta Diário ||
+            Qtde. Total: <?= $TotalCliContaD ?> ||
+            Total Clientes: <?= $TotalClientesCD ?>
+          </button>
           </h5>
-          <table class="table table-striped table-hover mb-0" id="Resultado" name="Resultado">
-            <thead>
-              <tr class="table-primary">
-                <th scope="col">Contrato</th>
-                <th scope="col">Cliente</th>
-                <th scope="col">CpfCnpj</th>
-                <th scope="col">Tipo Assinatura</th>
-                <th scope="col">Plano Pgto.</th>
-                <th scope="col">Dt. Assinatura</th>
-                <th scope="col">Vigencia</th>
-                <th scope="col">Tipo</th>
-                <th scope="col">Cidade</th>
-                <th scope="col">Dt. Nascimento</th>
-                <th scope="col">Sexo</th>
-                <th scope="col">Situação</th>
-              </tr>
-            </thead>
-            <tbody>
-              <?php foreach ($consultaDados as $key => $item): ?>
-                <tr>
-                  <td><?= $item['numeroDoContrato'] ?></td>
-                  <td><?= htmlspecialchars($item['NomeRazaoSocial']) ?></td>
-                  <td><?= $item['CpfCnpj'] ?></td>
-                  <td><?= $item['TipoAssinatura'] ?></td>
-                  <td><?= $item['PlanoPagto'] ?></td>
-                  <td><?= date('d/m/Y', strtotime($item['DataAssinatura'])) ?></td>
-                  <td><?= date('d/m/Y', strtotime($item['DataInicio'])) . ' - ' . date('d/m/Y', strtotime($item['DataFinal'])) ?></td>
-                  <td><?= $item['Tipo'] ?></td>
-                  <td><?= $item['Cidade'] ?></td>
-                  <td style="text-align: center;">
-                    <?= str_pad($item['diaDeNascimento'], 2, '0', STR_PAD_LEFT) . '/' . str_pad($item['mesDeNascimento'], 2, '0', STR_PAD_LEFT) . '/' . $item['anoDeNascimento'] ?>
-                  </td>
-                  <td style="text-align: center;"><?= $item['sexo'] ?></td>
-                  <td style="text-align: center;"><?= $item['SituacaoContrato'] ?></td>
-                </tr>
-              <?php endforeach; ?>
-            </tbody>
-          </table>
-        </div>
+          <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accodionAssinantes">
+            <div class="accordion-body">
+              <table class="table table-striped table-hover mb-0" id="Resultado" name="Resultado">
+                <thead>
+                  <tr class="table-primary">
+                    <th scope="col">Cliente</th>
+                    <th scope="col">CpfCnpj</th>
+                    <th scope="col">E-Mail</th>
+                    <th scope="col">Tipo Assinatura</th>
+                    <th scope="col">Dt. Assinatura</th>
+                    <th scope="col">Tipo</th>
+                    <th scope="col">Situação</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php foreach ($consultaContaDiario as $key => $item): ?>
+                    <tr>
+                      <td><?= htmlspecialchars($item['Cliente']) ?></td>
+                      <td></td>
+                      <td><?= htmlspecialchars($item['email']) ?></td>
+                      <td><?= htmlspecialchars($item['name']) ?></td>
+                      <td><?= date('d/m/Y', strtotime($item['createdAt'])) ?></td>
+                      <td><?= $item['type'] ?></td>
+                      <td><?= $item['status'] ?></td>
+                    </tr>
+                  <?php endforeach; ?>
+                </tbody>
+              </table>
+            </div>
+          </div>
+      </div>
+
+      <div class="accordion-item border rounded shadow-sm mb-3">
+        <h2 class="accordion-header" id="headingOne">
+          <button class="accordion-button collapsed bg-primary text-white fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseTwo">
+            <i class="bi bi-card-list me-2"></i>
+            Relação Clientes Holos ||
+            Qtde. Total: <?= $TotalCliHolos ?> ||
+            Total Clientes: <?= $TotalClientes ?>
+          </button>
+          </h5>
+          <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accodionAssinantes">
+            <div class="accordion-body">
+              <table class="table table-striped table-hover mb-0" id="Resultado" name="Resultado">
+                <thead>
+                  <tr class="table-primary">
+                    <th scope="col">Contrato</th>
+                    <th scope="col">Cliente</th>
+                    <th scope="col">CpfCnpj</th>
+                    <th scope="col">Tipo Assinatura</th>
+                    <th scope="col">Plano Pgto.</th>
+                    <th scope="col">Dt. Assinatura</th>
+                    <th scope="col">Vigencia</th>
+                    <th scope="col">Tipo</th>
+                    <th scope="col">Cidade</th>
+                    <th scope="col">Dt. Nascimento</th>
+                    <th scope="col">Sexo</th>
+                    <th scope="col">Situação</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php foreach ($consultaDados as $key => $item): ?>
+                    <tr>
+                      <td><?= $item['numeroDoContrato'] ?></td>
+                      <td><?= htmlspecialchars($item['NomeRazaoSocial']) ?></td>
+                      <td><?= $item['CpfCnpj'] ?></td>
+                      <td><?= $item['TipoAssinatura'] ?></td>
+                      <td><?= $item['PlanoPagto'] ?></td>
+                      <td><?= date('d/m/Y', strtotime($item['DataAssinatura'])) ?></td>
+                      <td><?= date('d/m/Y', strtotime($item['DataInicio'])) . ' - ' . date('d/m/Y', strtotime($item['DataFinal'])) ?></td>
+                      <td><?= $item['Tipo'] ?></td>
+                      <td><?= $item['Cidade'] ?></td>
+                      <td style="text-align: center;">
+                        <?= str_pad($item['diaDeNascimento'], 2, '0', STR_PAD_LEFT) . '/' . str_pad($item['mesDeNascimento'], 2, '0', STR_PAD_LEFT) . '/' . $item['anoDeNascimento'] ?>
+                      </td>
+                      <td style="text-align: center;"><?= $item['sexo'] ?></td>
+                      <td style="text-align: center;"><?= $item['SituacaoContrato'] ?></td>
+                    </tr>
+                  <?php endforeach; ?>
+                </tbody>
+              </table>
+            </div>
+          </div>
       </div>
     </div>
   </div>
+
 <?php endif; ?>
 
 <!-- Inclui JavaScript -->
--- public/classes/Functions/AnaliticoCirculacao.php
+++ public/classes/Functions/AnaliticoCirculacao.php
@@ -5,10 +5,12 @@ class AnaliticoCirculacao
 {
   // Conexões
   private $gestor;
+  private $contaDiario;
 
   public function __construct()
   {
     $this->gestor = DatabaseConnection::getConnection('gestor');
+    $this->contaDiario = DatabaseConnection::getConnection('contaDiario');
   }
 
   public function consultaContratos(): array
@@ -19,7 +21,11 @@ class AnaliticoCirculacao
           WHEN PP.descricaoDoPlanoDePagamento LIKE '%CBO%' THEN 'Combo'
           WHEN PP.descricaoDoPlanoDePagamento NOT LIKE '%CBO%' AND TA.codigoDoProdutoServico = 1 THEN 'Impresso'
           WHEN PP.descricaoDoPlanoDePagamento NOT LIKE '%CBO%' AND TA.codigoDoProdutoServico = 3 THEN 'Digital'
-        END AS Tipo, TA.codigoDoProdutoServico AS Produto,
+        END AS Tipo,
+        CASE 
+          WHEN PP.descricaoDoPlanoDePagamento LIKE '%CBO%' THEN '13'
+          ELSE TA.codigoDoProdutoServico
+        END AS Produto,
         C.dataDeValidadeInicial AS DataInicio,
         C.dataDevalidadeFinal as DataFinal,
         P.identMF AS CpfCnpj, 
@@ -43,16 +49,29 @@ class AnaliticoCirculacao
         INNER JOIN cadSituacao S WITH (NOLOCK) ON C.situacaoDoContrato = S.codigoSituacao
         WHERE C.dataDeValidadeInicial <= GETDATE() AND C.dataDevalidadeFinal >= GETDATE()
         AND C.situacaoDoContrato IN (1,3)
-
         GROUP BY C.numeroDoContrato, descricaoDoPlanoDePagamento, codigoDoProdutoServico, identMF, nomeRazaoSocial, nomeDoMunicipio, siglaDaUf, descricaoSituacao,
           dataDaAssinatura, valorTotal, T.valorDaParcela, situacaoDoContrato, diaDeNascimento, mesDeNascimento, anoDeNascimento, sexo,
           descricaoTipoDeAssinatura, descricaoDoPlanoDePagamento, 
           C.dataDeValidadeInicial, C.dataDevalidadeFinal
-
         ORDER BY NomeRazaoSocial, DataInicio
       ";
 
     $stmt = $this->gestor->prepare($sql);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+  public function consultaContaDiario(): array
+  {
+    $sql =
+      "SELECT c.name AS Cliente, c.email, c.createdAt, s.endAt, p.name, s.type, s.status
+        FROM customers c
+        INNER JOIN customer_details cd ON c.id = cd.customerId
+        INNER JOIN signatures s ON c.id = s.customerID
+        INNER JOIN products p ON s.productId = p.id
+        ORDER BY createdAt, c.name
+      ";
+    $stmt = $this->contaDiario->prepare($sql);
     $stmt->execute();
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
   }
