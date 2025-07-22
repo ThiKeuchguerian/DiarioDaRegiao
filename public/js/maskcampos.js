08311e50d7840976a9107cb9cfc1bca04246d971
Thiago Guimarães Corrêa Keuchguerian <thi.guimaraes@yahoo.com.br>
2025-07-22 16:46:01 -0300

    incluido mascara para campo cpfcnpj

--- public/js/maskcampos.js
+++ public/js/maskcampos.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
-  // Adiciona a máscara ao campo de MesAno
+  // Máscara para MesAno
   $('#MesAno').mask('00/0000');
+  
+  // Máscara para CPF/CNPJ - aceita apenas números
+  $('#cpfcnpj').on('input', function() {
+    var val = $(this).val().replace(/\D/g, '');
+    $(this).val(val); // força apenas números no campo
+    if (val.length <= 11) {
+      $(this).mask('000.000.000-00', {reverse: true});
+    } else {
+      $(this).mask('00.000.000/0000-00', {reverse: true});
+    }
+  });
 });
